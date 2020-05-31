8bedb61037648b8af9ee72905084c0dfb5205aae
abdulgaffersiam <magaffer03@gmail.com>
2020-06-01 00:20:00 +0900

    Spring IOC example program added

---
+++ springIOC/src/com/springproject/ioc/Grameen.java
@@ -0,0 +1,14 @@
+package com.springproject.ioc;
+
+public class Grameen implements Sim{
+	@Override
+	public void calling() {
+		System.out.println("calling from grameen");
+		
+	}
+	@Override
+	public void data() {
+		System.out.println("browsing internet using grameen sim");
+		
+	}
+}
